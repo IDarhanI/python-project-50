6c1fcc67924826a11f570d1c9da50a61b2e7cbbc
Денис Проскуряков <idarhani3567@gmail.com>
2025-09-19 15:37:35 +0600

    upgrade gendiff

--- hexlet_code/__init__.py
+++ hexlet_code/__init__.py
@@ -1,1 +1,3 @@
-__version__ = "0.1.0"
+from .scripts.gendiff import generate_diff
+
+__all__ = ['generate_diff']
--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -2,6 +2,36 @@ import argparse
 import json
 
 
+def format_value(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    elif value is None:
+        return "null"
+    else:
+        return str(value)
+
+
+def generate_diff(file_path1, file_path2):
+    data1 = json.load(open(file_path1))
+    data2 = json.load(open(file_path2))
+
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+
+
+    diff = []
+
+    for key in all_keys:
+        if key not in data2:
+            diff.append(f" - {key}: {format_value(data1[key])}")
+        elif key not in data1:
+            diff.append(f" + {key}: {format_value(data2[key])}")
+        elif data1[key] == data2[key]:
+            diff.append(f"    {key}: {format_value(data1[key])}")
+        else:
+            diff.append(f" - {key}: {format_value(data1[key])}")
+            diff.append(f" + {key}: {format_value(data2[key])}")
+    return "{\n" + "\n".join(diff) + "\n}"
+
 def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference.",
@@ -14,14 +44,8 @@ def main():
     
     args = parser.parse_args()
 
-    data1 = json.load(open(args.first_file))
-    
-    data2 = json.load(open(args.second_file))
-
-    print("first file", data1)
-    print("second file", data2)
-
-    return data1, data2
+    diff_generate = generate_diff(args.first_file, args.second_file)
+    print(diff_generate)
 
 if __name__ == "__main__":
     main()
